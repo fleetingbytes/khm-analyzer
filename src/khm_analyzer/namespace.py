9e49b2b470077572510a95e8b37449b97f16c2c9
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-08-09 03:27:52 +0200

    style: add alias for chain.from_iterable

--- src/khm_analyzer/namespace.py
+++ src/khm_analyzer/namespace.py
@@ -8,12 +8,13 @@ XML_NAMESPACE = "http://www.w3.org/XML/1998/namespace"
 NAMESPACE_MAP = { "ns": TEI_NAMESPACE }
 
 parentheses = ("{", "}")
+flatten = chain.from_iterable
 
 def any_namespace(tag: str) -> str:
-    return "".join(chain.from_iterable(zip(parentheses, (ANY_NAMESPACE, tag), strict=True)))
+    return "".join(flatten(zip(parentheses, (ANY_NAMESPACE, tag), strict=True)))
 
 def xml_namespace(tag: str) -> str:
-    return "".join(chain.from_iterable(zip(parentheses, (XML_NAMESPACE, tag), strict=True)))
+    return "".join(flatten(zip(parentheses, (XML_NAMESPACE, tag), strict=True)))
 
 def tei_namespace(tag: str) -> str:
-    return "".join(chain.from_iterable(zip(parentheses, (TEI_NAMESPACE, tag), strict=True)))
+    return "".join(flatten(zip(parentheses, (TEI_NAMESPACE, tag), strict=True)))
