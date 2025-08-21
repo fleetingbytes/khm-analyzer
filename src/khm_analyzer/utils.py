46f5844f9fa00377cdd8f5366ebc749e22f23ead
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-08-21 05:00:13 +0200

    feat: add logging decorator

--- src/khm_analyzer/utils.py
+++ src/khm_analyzer/utils.py
@@ -1,7 +1,8 @@
+from functools import wraps
 from io import IOBase, TextIOWrapper, BytesIO
 from os import SEEK_SET
 from pathlib import Path
-from logging import getLogger
+from logging import getLogger, DEBUG
 
 
 logger = getLogger(__name__)
@@ -16,3 +17,32 @@ def get_file_name_from_buffer(buffer: TextIOWrapper) -> str:
         return Path(buffer.name).name
     except AttributeError:
         return f"{buffer} Id: {id(buffer)}"
+
+def log_io(level=DEBUG):
+    """
+    Decorator factory that logs function input arguments and return values
+    at the specified logging level.
+
+    Usage:
+        debug = @log_io(logging.DEBUG)
+        info = @log_io(logging.INFO)
+
+        @debug
+        def my_func(...):
+            ...
+
+        @info
+        def more_important_func(...):
+            ...
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            logger.log(level, "Calling %s with args=%s, kwargs=%s", func.__name__, str(args), str(kwargs), stacklevel=2)
+            result = func(*args, **kwargs)
+            logger.log(level, "%s returned %r", func.__name__, result, stacklevel=2)
+            return result
+        return wrapper
+    return decorator
+
+debug = log_io(DEBUG)
