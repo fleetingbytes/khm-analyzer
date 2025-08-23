9e7b889de1fce5b75507f0f4f7485da3cf749d01
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-08-23 05:36:15 +0200

    fix: create path for downloading source files

--- src/khm_analyzer/cli/download_source.py
+++ src/khm_analyzer/cli/download_source.py
@@ -1,6 +1,11 @@
 from argparse import Namespace
+from pathlib import Path
 from ..utils import debug_in
 from ..download import get_download_link, get_source_document_as_raw_bytes
+
+
+def create_parent_dir_if_not_exists(file: Path) -> None:
+    file.parent.mkdir(parents=True, exist_ok=True)
 
 
 @debug_in
@@ -11,6 +16,8 @@ def download_source(args: Namespace) -> None:
 
     link = get_download_link(edition, volume)
     raw_bytes = get_source_document_as_raw_bytes(link)
+
+    create_parent_dir_if_not_exists(output_file)
 
     with output_file as file:
         file.write(raw_bytes)
