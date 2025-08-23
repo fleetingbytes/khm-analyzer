d2752e55663973dd0c14562f7140f98703cbba58
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-08-23 05:39:29 +0200

    fix: create path for downloading all source files

--- src/khm_analyzer/cli/download_all_sources.py
+++ src/khm_analyzer/cli/download_all_sources.py
@@ -2,6 +2,7 @@ from argparse import Namespace
 from itertools import product
 from ..download import EDITION_RANGE, VOLUME_RANGE, get_download_link, get_source_document_as_raw_bytes
 from ..utils import debug_in
+from .download_source import create_parent_dir_if_not_exists
 
 
 @debug_in
@@ -13,6 +14,8 @@ def download_all_sources(args: Namespace):
 
         link = get_download_link(edition, volume)
         raw_bytes = get_source_document_as_raw_bytes(link)
+
+        create_parent_dir_if_not_exists(path)
 
         with path.open("wb") as file:
             file.write(raw_bytes)
