e8b6563afa27291a628876ac4261b9daae9ea5fd
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-12-20 21:10:12 +0100

    fix: allow creation of parents directories for logging

--- src/khm_analyzer/cli/__init__.py
+++ src/khm_analyzer/cli/__init__.py
@@ -13,7 +13,7 @@ app_name = "khm-analyzer"
 author = "fleetingbytes"
 
 log_dir = Path(user_log_dir(app_name, author))
-log_dir.mkdir(exist_ok=True)
+log_dir.mkdir(parents=True, exist_ok=True)
 
 logging_config = create_dict_config(log_dir / "debug.log", app_name)
 configure_logging(logging_config)
