e2c0a87da106ae46495feccc0045580453fca73d
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-12-21 06:34:28 +0100

    test(fix): use a skip reason

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -6,7 +6,7 @@ from khm_analyzer.utils import set_stream_position_to_the_start
 from khm_analyzer.warnings import InvalidXmlCorrectedWarning
 
 
-@mark.skip()
+@mark.skip("No invalid XML available")
 @mark.parametrize(
     "khm_edition_volume",
     (param((3, 1), id="ed3-vol1"),),
